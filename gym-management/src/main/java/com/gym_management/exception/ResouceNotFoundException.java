9a577073da67f77e463f228ec09e8ee1857e156a
ngnhatminh1711 <ngnhatminh1711@gmail.com>
2025-03-27 03:39:16 +0700

    Delete gym-management folder.

--- gym-management/src/main/java/com/gym_management/exception/ResouceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.gym_management.exception;
-
-public class ResouceNotFoundException extends RuntimeException {
-    public ResouceNotFoundException(String message) {
-        super(message);
-    }
-}
--- gym-management/src/main/java/com/gym_management/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gym_management.repository;
-
-import com.gym_management.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    User findByEmail(String email);
-}
